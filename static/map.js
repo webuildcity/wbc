cc7f716b693fa154a0a449f25c7d54a5e1bfd751
Jochen Klar <jochenklar@googlemail.com>
2013-02-17 17:50:27 +0100

    Fix für Race-Conditionzwischen bootstrap und leaflet

--- static/map.js
+++ static/map.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-	
-	
+function initMap() {
 	//Die Karte wird in der index-html in das Div mit der id "mymap" gezeichnet
 	map = new L.Map("map");
 	
@@ -18,33 +16,33 @@ $(document).ready(function() {
 	map.setView(center, min);	
 	
 	var fernsehturm = L.icon({
-    iconUrl: '/static/img/Fernsehturm_grau_26px3.png',    
-    iconSize:     [27, 125], // size of the icon   
-    iconAnchor:   [8,111] // point of the icon which will correspond to marker's location       
+        iconUrl: '/static/img/Fernsehturm_grau_26px3.png',    
+        iconSize:     [27, 125], // size of the icon   
+        iconAnchor:   [8,111] // point of the icon which will correspond to marker's location       
 	});
 	
 	var fernsehturmMarker = L.marker([52.520841,13.409405],{icon: fernsehturm}).addTo(map);
 	
 	
 	var funkturm = L.icon({
-    iconUrl: '/static/img/Funkturm_36px_neu.png',    
-    iconSize:     [36, 110], // size of the icon   
-    iconAnchor:   [16,108] // point of the icon which will correspond to marker's location       
+        iconUrl: '/static/img/Funkturm_36px_neu.png',    
+        iconSize:     [36, 110], // size of the icon   
+        iconAnchor:   [16,108] // point of the icon which will correspond to marker's location       
 	});	
 	var funkturmMarker = L.marker([52.5050681,13.278211400000032],{icon: funkturm}).addTo(map);
 	
 	var greenIcon = L.icon({
-    iconUrl: '/static/img/marker_green.png',    
-    iconSize:     [21, 32], // size of the icon width,height    
-    iconAnchor:   [14, 45], // point of the icon which will correspond to marker's location    
-    popupAnchor:  [4, -20] // point from which the popup should open relative to the iconAnchor
+        iconUrl: '/static/img/marker_green.png',    
+        iconSize:     [21, 32], // size of the icon width,height    
+        iconAnchor:   [14, 45], // point of the icon which will correspond to marker's location    
+        popupAnchor:  [4, -20] // point from which the popup should open relative to the iconAnchor
 	});	    
     
     var redIcon = L.icon({
-    iconUrl: '/static/img/marker_red.png',    
-    iconSize:     [21, 32], // size of the icon width,height    
-    iconAnchor:   [14, 45], // point of the icon which will correspond to marker's location    
-    popupAnchor:  [4, -20] // point from which the popup should open relative to the iconAnchor
+        iconUrl: '/static/img/marker_red.png',    
+        iconSize:     [21, 32], // size of the icon width,height    
+        iconAnchor:   [14, 45], // point of the icon which will correspond to marker's location    
+        popupAnchor:  [4, -20] // point from which the popup should open relative to the iconAnchor
 	});
 	
 	
@@ -60,8 +58,6 @@ $(document).ready(function() {
         
     });
     
-    
-
     $.each(projects, function(key,project){
         var lon = project.fields.lon;
         var lat = project.fields.lat;
@@ -70,4 +66,8 @@ $(document).ready(function() {
         marker.bindPopup(t);       
         
     });
+}
+
+$(document).ready(function() {
+    setTimeout('initMap()',100);
 });
