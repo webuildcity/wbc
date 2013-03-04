5fcf8a00b464810a26c456738c962372694b5091
Magdalena Noffke <magdalena.noffke@googlemail.com>
2013-03-04 15:01:56 +0100

    links angepasst

--- bbs/urls.py
+++ bbs/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bbs/(?P<id>\d+)/$', 'projects.views.show'),
+    url(r'^(?P<id>\d+)/$', 'projects.views.show'),
     
 )
