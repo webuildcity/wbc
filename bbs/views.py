fc579b470837e300c3066033ecd2e9612c62ffe9
Magdalena Noffke <magdalena.noffke@googlemail.com>
2013-03-07 13:15:17 +0100

    filterfunktion eingefügt

--- bbs/views.py
+++ bbs/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.core import serializers
-
 from projects.models import Project,BBP
+import datetime
 
 def home(request):
     p = Project.objects.all()
-    b = BBP.objects.all()
+    b = BBP.objects.filter(end__gte = datetime.date.today())    
     projectsJson = serializers.serialize("json", p)
     bbpJson = serializers.serialize("json", b) 
     return render(request,'bbs/map.html',{
