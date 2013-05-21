41398f4a26b0185f8e698ec9508a1c4436179619
Jochen Klar <jochenklar@googlemail.com>
2013-05-21 22:58:18 +0200

    social buttons, new popups, and old projects button

--- bbs/urls.py
+++ bbs/urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
     url(r'^info/$', 'bbs.views.info'),
     # admin foo
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)), 
+    url(r'^admin/', include(admin.site.urls))
 )
