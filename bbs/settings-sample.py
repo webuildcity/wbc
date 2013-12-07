b6c974769f6d1542a40f5e30198aa1450fd87cfb
Jochen Klar <mail@jochenklar.de>
2013-12-07 16:02:23 +0100

    static files re-organized

--- bbs/settings-sample.py
+++ bbs/settings-sample.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT,'collected/')
+STATIC_ROOT = os.path.join(SITE_ROOT,'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,10 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT,'static/'),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT,'bbs/static/'),
 )
 
 # List of finder classes that know how to find static files in
--- bbs/static/map.js
+++ bbs/static/map.js
@@ -72,7 +72,7 @@ function initMap() {
         popuptext += '<p>Beteiligung möglich bis: ' + point.ende + '</p>';
         popuptext += '<p><a href="' + siteUrl + "orte/" + point.projekt + '" >Details</a></p>';
 
-        marker.bindPopup(popuptext, {autoPanPaddingTopLeft: new L.Point(10,180), autoPanPaddingBottomRight: new L.Point(10,0)});
+        marker.bindPopup(popuptext, {autoPanPaddingTopLeft: new L.Point(10,100), autoPanPaddingBottomRight: new L.Point(10,0)});
         marker.addTo(map);
 
         layers[point.vspk].layerGroup.addLayer(marker);
@@ -108,7 +108,7 @@ function initMap() {
         popuptext += '<p>Beteiligung war möglich bis: ' + point.ende + '</p>';
         popuptext += '<p><a href="' + siteUrl + "orte/" + point.projekt + '" >Details</a></p>';
         
-        marker.bindPopup(popuptext, {autoPanPaddingTopLeft: new L.Point(10,180), autoPanPaddingBottomRight: new L.Point(10,0)});        
+        marker.bindPopup(popuptext, {autoPanPaddingTopLeft: new L.Point(10,100), autoPanPaddingBottomRight: new L.Point(10,0)});        
         
         marker.on("mouseover", function(e) {
             e.target._icon.src = staticUrl + '/img/Baustellenschilder/klein/schild_grau.png';
@@ -133,7 +133,7 @@ function initMap() {
     var info = $('<div />', {
         'class': 'leaflet-control-zoom leaflet-bar leaflet-control pull-left',
         'html': '<a id="info-button" class="leaflet-control-zoom-out" href="#" title="Info">i</a>'
-    }).appendTo($('#buttons-right'));
+    }).appendTo($('#buttons-left'));
 
     $('#info-button').on('click', function () {
         alert('INFO!');
