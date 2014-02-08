4b8d522c5e5e579720aa1219a701e4107fcfbc27
Jochen Klar <git@jochenklar.de>
2014-02-08 11:01:55 +0100

    some testing

---
+++ projekte/tests.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from projekte.models import Projekt, Veroeffentlichung, Verfahrensschritt, Verfahren, Behoerde, Bezirk
+
+class ProjektTestCase(TestCase):
+    def test_bezirke(self):
+        a = Bezirk.objects.get(name='Mitte')
+        b = Bezirk.objects.all()
+        self.assertIn(a,b)
+
+    def test_behoerde(self):
+        a = Behoerde.objects.get(name='Mitte')
+        b = Behoerde.objects.all()
+        self.assertIn(a,b)
+
+    def test_verfahrensschritt(self):
+        a = Verfahrensschritt.objects.get(name='')
+        b = Verfahrensschritt.objects.all()
+        self.assertIn(a,b)
+
+    def test_verfahren(self):
+        a = Verfahren.objects.get(name='')
+        b = Verfahren.objects.all()
+        self.assertIn(a,b)
+
+    def test_projekt(self):
+        # store ne project
+        a = Projekt(adresse='Unter den Linden 1',beschreibung='Brandenburger Tor',lat='-13',lon='52',bezeichner='ACB')
+        a.save()
+        b = Bezirk.objects.get(name='Mitte')
+        a.bezirke.add(b)
+        a.save()
+
+        # retrieve project
+        c = Projekt.objects.get(bezeichner='ACB')
+        self.assertIn(b,c.bezirke.all())
+
+    def test_veroeffentlichung(self):
+        pass
