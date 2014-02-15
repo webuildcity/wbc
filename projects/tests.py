7d11bf6d9f9bc6d90fd120272d2eb3e2975975c3
Jochen Klar <mail@jochenklar.de>
2014-02-15 14:48:16 +0100

    tests updated

--- projects/tests.py
+++ projects/tests.py
@@ -25,38 +25,10 @@ class ProjectsTestCase(TestCase):
         )
         c.save()
 
-    def test_bezirke(self):
-        a = Bezirk.objects.get(pk=1)
-        b = Bezirk.objects.all()
-        self.assertIn(a,b)
-
-    def test_behoerde(self):
-        a = Behoerde.objects.get(pk=1)
-        b = Behoerde.objects.all()
-        self.assertIn(a,b)
-
-    def test_verfahrensschritt(self):
-        a = Verfahrensschritt.objects.get(pk=1)
-        b = Verfahrensschritt.objects.all()
-        self.assertIn(a,b)
-
-    def test_verfahren(self):
-        a = Verfahren.objects.get(pk=1)
-        b = Verfahren.objects.all()
-        self.assertIn(a,b)
-
     def test_ort(self):
-        a = Ort.objects.get(pk=1)
-        b = Ort.objects.all()
-        self.assertIn(a,b)
-
-        response = Client().get('/orte/')
+        client = Client()
+        response = client.get('/orte/')
         self.assertEqual(response.status_code,200)
 
-        response = Client().get('/orte/1/')
+        response = client.get('/orte/1/')
         self.assertEqual(response.status_code,200)
-
-    def test_veroeffentlichung(self):
-        a = Veroeffentlichung.objects.get(pk=1)
-        b = Veroeffentlichung.objects.all()
-        self.assertIn(a,b)
