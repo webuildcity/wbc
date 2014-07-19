98a974143e043c30d5e655fdc8d531d87f6de9e2
Jochen Klar <jochenklar@gmail.com>
2014-07-19 22:44:53 +0200

    feed for veröffentlichungen

--- bbs/views.py
+++ bbs/views.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.core import serializers
-from django.http import HttpResponseRedirect
-from projects.models import Ort, Veroeffentlichung, Verfahrensschritt, Verfahren, Behoerde, Bezirk
-from bbs.forms import LoginForm
+from django.http import HttpResponseRedirect,HttpResponse
 from django.shortcuts import Http404,render_to_response,redirect,render
 from django.contrib.auth import authenticate, login, logout
-from bbs.forms import FindOrt,CreateVeroeffentlichung
-from django.template import RequestContext
-import datetime
-from django.http import HttpResponse
-import json
 from django.contrib.auth.decorators import login_required
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Rss201rev2Feed
+from django.template import RequestContext
+
+from forms import LoginForm,FindOrt,CreateVeroeffentlichung
+from projects.models import Ort, Veroeffentlichung, Verfahrensschritt, Verfahren, Behoerde, Bezirk
+
+import datetime,json
 
 def home(request):
     return render(request,'bbs/map.html')
@@ -69,3 +70,31 @@ def create_veroeffentlichung(request):
         else:
             form = CreateVeroeffentlichung(initial={'ort': ort})
             return render(request,'bbs/create_veroeffentlichung_step2.html',{'form':form})
+
+class VeroeffentlichungenFeedMimeType(Rss201rev2Feed):
+    mime_type = 'application/xml'
+
+class VeroeffentlichungenFeed(Feed):
+    title = "Bürger baut Stadt (Veröffentlichungen)"
+    description = "Veröffentlichungen zu Bauvorhaben in Berlin"
+    link = "http://buergerbautstadt.de"
+    feed_url = "http://buergerbautstadt.de/veroeffentlichungen/feed/"
+    feed_type = VeroeffentlichungenFeedMimeType
+
+    def items(self):
+        return Veroeffentlichung.objects.order_by('-created')[:20]
+
+    def item_title(self, item):
+        return item.verfahrensschritt.verfahren.name + ': ' +  item.verfahrensschritt.name + ' (' + item.ort.bezeichner + ', ' + item.ort.bezirke.all()[0].name + ')'
+
+    def item_description(self, item):
+        return item.beschreibung
+
+    def item_guid(self, item):
+        return str(item.pk)
+
+    def item_pubdate(self, item):
+        return item.created
+
+    def item_link(self, item):
+        return 'http://localhost:8000/orte/' + str(item.ort.pk)
