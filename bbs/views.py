35192ba24c41a34bd0a7d600a5e30795b0bc4e9f
Jochen Klar <jochenklar@gmail.com>
2014-07-19 22:46:28 +0200

    small fix

--- bbs/views.py
+++ bbs/views.py
@@ -82,7 +82,7 @@ class VeroeffentlichungenFeed(Feed):
     feed_type = VeroeffentlichungenFeedMimeType
 
     def items(self):
-        return Veroeffentlichung.objects.order_by('-created')[:20]
+        return Veroeffentlichung.objects.order_by('-created')[:10]
 
     def item_title(self, item):
         return item.verfahrensschritt.verfahren.name + ': ' +  item.verfahrensschritt.name + ' (' + item.ort.bezeichner + ', ' + item.ort.bezirke.all()[0].name + ')'
