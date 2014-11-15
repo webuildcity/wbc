502aed9c31514fe06243ee1bfadc5b686c68d320
Jochen Klar <jochenklar@gmail.com>
2014-11-15 15:12:45 +0100

    local setting file introduced

---
+++ bbs/default.local.py
@@ -0,0 +1,61 @@
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+SECRET_KEY = 'this is a not very secret key'
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': '',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',
+    #     'PORT': '',
+    # }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': '',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '',
+    #     'PORT': '',
+    # }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': '' # path to database file
+    # }
+}
+
+# SITE_ROOT = os.path.dirname(os.path.dirname(__file__))
+# SITE_URL = 'http://localhost:8000'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = '25'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_FROM = 'news@buergerbautstadt.de'
+
+# TILES_URL = 'http://tiles.codefor.de/static/bbs/berlin/'
+# TILES_OPT = {
+#     'errorTileUrl': 'http://tiles.codefor.de/static/bbs/error.png',
+#     'attribution': 'Map data &copy; 2012 OpenStreetMap contributors',
+#     'maxZoom': 17,
+#     'minZoom': 9,
+#     'zIndex': 0,
+#     'reuseTiles': True
+# }
+
+# DEFAULT_VIEW = {
+#     'lat': 52.51,
+#     'lon': 13.37628,
+#     'zoom': 11
+# }
---
+++ bbs/settings.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+import os
+from local import *
+
+INSTALLED_APPS = (
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.formtools',
+    # wagtail
+    'wagtail.wagtailcore',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailforms',
+    # other dependencies
+    'rest_framework',
+    'widget_tweaks',
+    'south',
+    'compressor',
+    'taggit',
+    'modelcluster',
+    # bbs apps
+    'projects',
+    'news',
+    'blog',
+    'comments'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+    'wagtail.wagtailredirects.middleware.RedirectMiddleware'
+)
+
+ROOT_URLCONF = 'bbs.urls'
+
+WSGI_APPLICATION = 'bbs.wsgi.application'
+
+SITE_ID = 1
+
+LANGUAGE_CODE = 'de-de'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(SITE_ROOT,'media/')
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(SITE_ROOT,'static/')
+
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT,'bbs/static/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(SITE_ROOT,'bbs/templates/'),
+    os.path.join(SITE_ROOT,'blog/templates/'),
+)
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+    'django.core.context_processors.request',
+    'bbs.context_processors.settings'
+)
+
+LOGIN_URL = '/login'
+
+WAGTAIL_SITE_NAME = 'Bürger baut Stadt'
+
+COMPRESS_ENABLED = True
+
+COMPRESS_PRECOMPILERS = ( 
+    ('text/x-scss', 'django_libsass.SassCompiler'), 
+)
