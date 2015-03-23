e5cd12e7782215880d043031385cffbe4a15c7f7
Jochen Klar <jochenklar@gmail.com>
2015-03-23 21:47:47 +0100

    social buttons moved to core/layout_social.html, map options moved to options.js

--- lib/wbc/core/context_processors.py
+++ lib/wbc/core/context_processors.py
@@ -1,39 +1,12 @@
 # -*- coding: utf-8 -*-
-import json
-
 from django.conf import settings as django_settings
-from django.utils.http import urlencode
 
 def settings(request):
-    twitter = '<a href="https://twitter.com/share" target="blank"><i class="fa fa-twitter-square"></i></a>';
-
     try:
         facebookapikey = django_settings.FACEBOOK_APIKEY
     except AttributeError:
-        facebookapikey = None
-
-    if facebookapikey:
-        facebook_params = urlencode({
-            'app_id': facebookapikey,
-            'display': 'page',
-            'redirect_uri': django_settings.SITE_URL,
-            'href': django_settings.SITE_URL
-        })
-        facebook = '<a href="https://www.facebook.com/dialog/share?%s" target="blank"><i class="fa fa-facebook-square"></i></a>' % facebook_params
-    else:
-        facebook = ''
-
-    gplus_params = urlencode({
-        'url': django_settings.SITE_URL,
-        'href': django_settings.SITE_URL
-    })
-    gplus = '<a href="https://plus.google.com/share?%s" target="blank"><i class="fa fa-google-plus-square"></i></a>'  % gplus_params
+        facebookapikey = ''
 
     return {
-        'tilesUrl': django_settings.TILES_URL,
-        'tilesOpt': json.dumps(django_settings.TILES_OPT),
-        'defaultView': json.dumps(django_settings.DEFAULT_VIEW),
-        'twitter': twitter,
-        'facebook': facebook,
-        'gplus': gplus
+        'facebook_apikey': facebookapikey
     }
