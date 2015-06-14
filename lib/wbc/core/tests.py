b26a68728f481158cf7302da936a1beda191c270
Magdalena Noffke <noffke@mi.fu-berlin.de>
2015-06-14 22:49:29 +0200

    add unit tests tp core app (not sure how to test abstract model)

---
+++ lib/wbc/core/tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+from models import *
+
+
+class CoreTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_comment(self):
+        pass
