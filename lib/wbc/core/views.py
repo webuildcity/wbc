84177bcaf760cc18759827423a9a0e5349c9f7ed
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-07-04 12:58:51 +0200

    bugfix

--- lib/wbc/core/views.py
+++ lib/wbc/core/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponseRedirect
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render, render_to_response
 from django.core.urlresolvers import reverse
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
@@ -11,12 +11,14 @@ from django.utils.decorators import method_decorator
 from wbc.core.forms import LoginForm
 from wbc.region.models import District
 
+
 def feeds(request):
     entities = District.objects.all()
     return render(request, 'core/feeds.html', {
         'entities': entities,
         'publication_feed_url': reverse('publication_feed_url')
     })
+
 
 def login_user(request):
     form = LoginForm(request.POST or None)
@@ -25,13 +27,13 @@ def login_user(request):
             user = form.login(request)
             if user:
                 login(request, user)
-                if request.GET.get('next'):
-                    # Redirect to a success page.
-                    return HttpResponseRedirect(request.GET.get('next'))
+                if request.POST.get('next'):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 else:
                     return HttpResponseRedirect('/')
 
     return render(request, 'core/login.html', {'form': form})
+
 
 def logout_user(request):
     logout(request)
