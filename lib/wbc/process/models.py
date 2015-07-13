9e756137141f17925d3c6302e1fd444d240328c2
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-07-13 19:06:23 +0200

    pep8

--- lib/wbc/process/models.py
+++ lib/wbc/process/models.py
@@ -3,18 +3,25 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 from wbc.core.models import Model
-from wbc.region.models import Entity,Department
+from wbc.region.models import Entity, Department
+from wbc.buildings.models import Building
+
 
 class Place(Model):
-    identifier  = models.CharField(blank=True, max_length=64, verbose_name="Bezeichner", help_text="ggf. Bezeichner des Beplauungsplans")
-    address     = models.CharField(max_length=256, blank=True, verbose_name="Adresse", help_text="Eine genaue Adresse des Vorhabens")
-    description = models.TextField(blank=True, verbose_name="Beschreibung", help_text="Örtliche Beschreibung")
-    entities    = models.ManyToManyField(Entity, blank=True, verbose_name="Einheit", related_name='places')
-    lat         = models.FloatField(verbose_name="Breitengrad")
-    lon         = models.FloatField(verbose_name="Längengrad")
-    polygon     = models.TextField(null=True, blank=True)
-    active      = models.BooleanField()
-    link        = models.URLField(blank=True)
+    identifier = models.CharField(
+        blank=True, max_length=64, verbose_name="Bezeichner", help_text="ggf. Bezeichner des Beplauungsplans")
+    address = models.CharField(
+        max_length=256, blank=True, verbose_name="Adresse", help_text="Eine genaue Adresse des Vorhabens")
+    description = models.TextField(
+        blank=True, verbose_name="Beschreibung", help_text="Örtliche Beschreibung")
+    entities = models.ManyToManyField(
+        Entity, blank=True, verbose_name="Einheit", related_name='places')
+    lat = models.FloatField(verbose_name="Breitengrad")
+    lon = models.FloatField(verbose_name="Längengrad")
+    polygon = models.TextField(null=True, blank=True)
+    active = models.BooleanField()
+    link = models.URLField(blank=True)
+    building = models.ForeignKey(Building, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('place', kwargs={'pk': self.pk})
@@ -35,20 +42,25 @@ class Place(Model):
         return ', '.join(strings)
 
     class Meta:
-        ordering            = ("-created",)
-        verbose_name        = "Ort"
+        ordering = ("-created",)
+        verbose_name = "Ort"
         verbose_name_plural = "Orte"
 
+
 class Publication(Model):
-    process_step = models.ForeignKey('ProcessStep', related_name='publications', verbose_name="Verfahrensschritt")
-    description  = models.TextField(blank=True, verbose_name="Beschreibung")
-    place        = models.ForeignKey('Place', related_name='publications', verbose_name="Ort")
-    begin        = models.DateField(verbose_name="Beginn der Auslegungszeit")
-    end          = models.DateField(verbose_name="Ende der Auslegungszeit")
-    office       = models.TextField(blank=True, verbose_name="Auslegungsstelle")
-    office_hours = models.TextField(blank=True, verbose_name="Öffnungszeiten der Auslegungsstelle")
-    department   = models.ForeignKey(Department, verbose_name="Verantwortliche Behörde")
-    link         = models.URLField(blank=True)
+    process_step = models.ForeignKey(
+        'ProcessStep', related_name='publications', verbose_name="Verfahrensschritt")
+    description = models.TextField(blank=True, verbose_name="Beschreibung")
+    place = models.ForeignKey(
+        'Place', related_name='publications', verbose_name="Ort")
+    begin = models.DateField(verbose_name="Beginn der Auslegungszeit")
+    end = models.DateField(verbose_name="Ende der Auslegungszeit")
+    office = models.TextField(blank=True, verbose_name="Auslegungsstelle")
+    office_hours = models.TextField(
+        blank=True, verbose_name="Öffnungszeiten der Auslegungsstelle")
+    department = models.ForeignKey(
+        Department, verbose_name="Verantwortliche Behörde")
+    link = models.URLField(blank=True)
 
     def get_absolute_url(self):
         return reverse('place', kwargs={'pk': self.place.pk})
@@ -63,33 +75,38 @@ class Publication(Model):
         return unicode(self.place) + ', ' + self.process_step.name
 
     class Meta:
-        ordering            = ("-end",)
-        verbose_name        = "Veröffentlichung"
+        ordering = ("-end",)
+        verbose_name = "Veröffentlichung"
         verbose_name_plural = "Veröffentlichungen"
 
+
 class ProcessStep(Model):
-    name         = models.CharField(max_length=256, verbose_name="Name")
-    description  = models.TextField(verbose_name="Beschreibung")
-    icon         = models.CharField(max_length=256, verbose_name="Icon auf der Karte")
-    hover_icon   = models.CharField(max_length=256, verbose_name="Icon auf der Karte bei Hovereffekt")
-    order        = models.IntegerField(verbose_name="Reihenfolge", help_text="Nummer in der Reihenfolge")
-    process_type = models.ForeignKey('ProcessType', related_name='process_steps', verbose_name="Verfahren")
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Beschreibung")
+    icon = models.CharField(max_length=256, verbose_name="Icon auf der Karte")
+    hover_icon = models.CharField(
+        max_length=256, verbose_name="Icon auf der Karte bei Hovereffekt")
+    order = models.IntegerField(
+        verbose_name="Reihenfolge", help_text="Nummer in der Reihenfolge")
+    process_type = models.ForeignKey(
+        'ProcessType', related_name='process_steps', verbose_name="Verfahren")
 
     def __unicode__(self):
         return unicode(self.process_type) + ', ' + self.name
 
     class Meta:
-        ordering            = ("process_type","order")
-        verbose_name        = "Verfahrensschritt"
+        ordering = ("process_type", "order")
+        verbose_name = "Verfahrensschritt"
         verbose_name_plural = "Verfahrensschritte"
 
+
 class ProcessType(Model):
-    name         = models.CharField(max_length=256, verbose_name="Name")
-    description  = models.TextField(verbose_name="Beschreibung")
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Beschreibung")
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name        = "Verfahren"
+        verbose_name = "Verfahren"
         verbose_name_plural = "Verfahren"
--- lib/wbc/region/models.py
+++ lib/wbc/region/models.py
@@ -3,49 +3,54 @@ from django.db import models
 
 from wbc.core.models import Model
 
+
 class Entity(Model):
-    name        = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
-    lat         = models.FloatField(null=True,blank=True)
-    lon         = models.FloatField(null=True,blank=True)
-    polygon     = models.TextField(blank=True)
+    lat = models.FloatField(null=True, blank=True)
+    lon = models.FloatField(null=True, blank=True)
+    polygon = models.TextField(blank=True)
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name        = 'Einheit'
+        verbose_name = 'Einheit'
         verbose_name_plural = 'Einheiten'
 
+
 class Muncipality(Entity):
+
     class Meta:
-        verbose_name        = 'Gemeinde'
+        verbose_name = 'Gemeinde'
         verbose_name_plural = 'Gemeinden'
+
 
 class District(Entity):
     muncipality = models.ForeignKey(Muncipality, related_name='districts')
 
     class Meta:
-        verbose_name        = 'Bezirk'
+        verbose_name = 'Bezirk'
         verbose_name_plural = 'Bezirke'
+
 
 class Quarter(Entity):
     district = models.ForeignKey(District, related_name='quarters')
 
     class Meta:
-        verbose_name        = 'Ortsteil'
+        verbose_name = 'Ortsteil'
         verbose_name_plural = 'Ortsteile'
 
+
 class Department(Model):
-    name        = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
-    link        = models.URLField(blank=True)
-    entity      = models.ForeignKey(Entity, related_name='departments')
+    link = models.URLField(blank=True)
+    entity = models.ForeignKey(Entity, related_name='departments')
 
     def __unicode__(self):
         return self.name
 
     class Meta:
-        verbose_name        = 'Behörde'
+        verbose_name = 'Behörde'
         verbose_name_plural = 'Behörden'
-
