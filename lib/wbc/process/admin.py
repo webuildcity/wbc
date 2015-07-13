379c9fb51acfb3bc976d996df94b25eb0b830915
Magdalena Noffke <magdalena.noffke@googlemail.com>
2015-07-13 19:05:53 +0200

    add building in admin

--- lib/wbc/process/admin.py
+++ lib/wbc/process/admin.py
@@ -4,15 +4,19 @@ from django.contrib import admin
 
 from models import *
 
+
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('id','identifier','address','active')
-    list_display_links = ('id','identifier','address','active')
-    fields = ['active','address','entities','lat','lon','description','identifier','link','polygon']
+    list_display = ('id', 'identifier', 'address', 'active')
+    list_display_links = ('id', 'identifier', 'address', 'active')
+    fields = ['active', 'address', 'entities', 'lat', 'lon',
+              'description', 'identifier', 'link', 'polygon', 'building']
     ordering = ['id']
     change_form_template = 'process/admin/change_form.html'
     add_form_template = 'process/admin/change_form.html'
 
+
 class PublicationAdminForm(forms.ModelForm):
+
     class Meta:
         model = Publication
         fields = '__all__'
@@ -21,20 +25,23 @@ class PublicationAdminForm(forms.ModelForm):
         super(PublicationAdminForm, self).__init__(*args, **kwds)
         self.fields['process_step'].queryset = ProcessStep.objects
 
+
 class PublicationAdmin(admin.ModelAdmin):
-    list_display = ('id','process_step','place','end')
-    list_display_links = ('id','process_step','place','end')
+    list_display = ('id', 'process_step', 'place', 'end')
+    list_display_links = ('id', 'process_step', 'place', 'end')
     ordering = ['id']
     form = PublicationAdminForm
 
+
 class ProcessStepAdmin(admin.ModelAdmin):
-    list_display = ('id','name','order','process_type')
-    list_display_links = ('id','name','order','process_type')
+    list_display = ('id', 'name', 'order', 'process_type')
+    list_display_links = ('id', 'name', 'order', 'process_type')
     ordering = ['id']
 
+
 class ProcessTypeAdmin(admin.ModelAdmin):
-    list_display = ('id','name')
-    list_display_links = ('id','name')
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
     ordering = ['id']
 
 admin.site.register(Place, PlaceAdmin)
