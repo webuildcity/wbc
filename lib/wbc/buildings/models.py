420b2a43173c57ae84ec6fba987c2439ca1f7079
Jochen Klar <jochenklar@gmail.com>
2015-08-02 10:21:02 +0200

    exclude region

--- lib/wbc/buildings/models.py
+++ lib/wbc/buildings/models.py
@@ -8,3 +8,6 @@ class Building(Model):
     exclude_region = models.TextField(null=True)
     model = models.FileField(upload_to='models')
     additional_file = models.FileField(upload_to='other')
+
+    def __unicode__(self):
+        return unicode(self.model)
