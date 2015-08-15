1512bc37b5ecd40b7e69692a02f75873ff323eaa
Jochen Klar <jochenklar@gmail.com>
2015-08-15 17:26:16 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('README.md') as f: description = f.read()
+version = '0.1.1'
 
 install_requires = [
     'Django==1.8',
     'django-widget-tweaks==1.3',
     'markdown==2.3.1',
-    'psycopg2>=2.5.1,<2.6',
-    'djangorestframework>=3.1,<3.2',
-    'djangorestframework-gis'
+    'djangorestframework==3.1',
+    'djangorestframework-gis',
 ]
 
 setup(
-    name='we-build-city',
+    name='webuildcity',
+    version=version,
     url='https://github.com/webuildcity/wbc/',
-    version='0.1.0',
+    download_url='https://github.com/webuildcity/wbc/archive/%s.tar.gz' % version,
     packages=find_packages(),
     license=u'GNU Lesser General Public License v3 (LGPLv3)',
-    author=u'Magdalena Noffke, Jochen Klar, Timo Lundelius',
-    maintainer=u'Magdalena Noffke, Jochen Klar, Timo Lundelius',
-    maintainer_email=u'info@we-build.city',
-    description=u'Finde geplante Bauvorhaben in deinem Kiez.',
-    long_description=description,
+    author=u'Magdalena Noffke, Jochen Klar, Timo Lundelius, Umut Tas, Volker Eichhorn',
+    author_email='info@we-build.city',
+    maintainer=u'Jochen Klar',
+    maintainer_email=u'jochenklar@gmail.com',
+    description=u'We-build.city is a Django module for a collaboration platform about participation in urban planning.',
+    long_description=u'We-build.city is a collaboration platform designed to enable everyone to participate in urban planning more easily. It encourages citizens, companies, public authorities and any other stakeholder to engage and cooperate in the urban planning process in order to reduce frustration, and possible lengthy and costly conflicts which often arise',
     include_package_data=True,
     install_requires=install_requires,
     classifiers=[
