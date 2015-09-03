0b07ed2a4b0a99c1aa4d4c277bcf718662fbdbcb
umut0 <umut0815@yahoo.com>
2015-09-03 18:50:29 +0200

    load fixtures

--- wbc/core/management/commands/load-fixtures.py
+++ wbc/core/management/commands/load-fixtures.py
@@ -14,7 +14,7 @@ fixtures = [
     'process/processtypes.json',
     'process/processsteps.json',
     # projects
-    'projects/project.json'
+    'projects/projects.json'
     # comments
     'comments/comments.json',
     # news
