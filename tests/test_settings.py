e28bdbb7a0778d9d2aebbf2ef311afb2ec3ea329
Jochen Klar <jochenklar@gmail.com>
2015-09-07 21:22:59 +0200

    tests directory and runtests.py

---
+++ tests/test_settings.py
@@ -0,0 +1,62 @@
+SECRET_KEY = 'this is a not very secret key'
+
+SITE_URL = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/tmp/wbc.sqlite3'
+    }
+}
+
+INSTALLED_APPS = [
+    # django
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    # other dependencies
+    'rest_framework',
+    'rest_framework_gis',
+    'widget_tweaks',
+    'markdown',
+    # we build city apps
+    'wbc.core',
+    'wbc.region',
+    'wbc.process',
+    'wbc.news',
+    'wbc.comments',
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'tests.urls'
+WSGI_APPLICATION = 'bbs.wsgi.application'
+
+LANGUAGE_CODE = 'de-de'
+TIME_ZONE = 'Europe/Berlin'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_FROM = 'info@example.com'
+
+FEED_TITLE = "Test Feed"
+FEED_DESCRIPTION = "Test Feed Description"
+
