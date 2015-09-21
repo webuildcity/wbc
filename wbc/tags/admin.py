d7d67f31f2275cb272a38a6df32c813da551699b
umut0 <umut0815@yahoo.com>
2015-09-21 15:16:43 +0200

    name change

--- wbc/tags/admin.py
+++ wbc/tags/admin.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 
 from models import *
+from taggit.admin import *
 
-class TagAdmin(admin.ModelAdmin):
+class WbcTagAdmin(admin.ModelAdmin):
     list_display = ('slug','name')
     list_display_links = ('slug','name')
     ordering = ['slug']
 
-admin.site.register(WbcTag, TagAdmin)
+# admin.site.unregister(TagAdmin)
+admin.site.register(WbcTag, WbcTagAdmin)
