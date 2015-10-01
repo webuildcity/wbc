b53a778ce28bfac1fc3792c1030af7c162c9e49d
Volker Eichhorn <mail@volker.io>
2015-10-01 12:22:52 +0200

    index for stakeholder

--- wbc/stakeholder/search_indexes.py
+++ wbc/stakeholder/search_indexes.py
@@ -1,36 +1,32 @@
-
 from rest_framework.reverse import reverse
 
 from haystack import indexes
-from models import Project
+from models import Stakeholder
 
 
-class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
+class StakeholderIndex(indexes.SearchIndex, indexes.Indexable):
 
     text = indexes.CharField(document=True, use_template=True)
     name = indexes.CharField(model_attr='name')
     description = indexes.CharField(model_attr='description')
-    location = indexes.LocationField()
-    polygon = indexes.CharField(model_attr='polygon', null=True)
     active = indexes.BooleanField(model_attr='active')
     tags = indexes.MultiValueField()
-    api_url = indexes.CharField()
+    roles = indexes.MultiValueField()
     type = indexes.CharField()
 
     content_auto = indexes.NgramField(use_template=True)
 
     def get_model(self):
-        return Project
+        return Stakeholder
 
     def prepare_tags(self, obj):
         return [tag.name for tag in obj.tags.all()]
 
-    def prepare_location(self, obj):
-        # If you're just storing the floats...
-        return "%s,%s" % (obj.lon, obj.lat)
-
     def prepare_type(self, obj):
-        return 'project'
+        return 'stakeholder'
+
+    def prepare_roles(self, obj):
+        return [role.name for role in obj.roles.all()]
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
