92c0ce396d4fc8cefe110bd7cf5dc0f6c9859b1d
umut0 <umut0815@yahoo.com>
2015-11-06 20:38:12 +0100

    search directive

--- wbc/core/static/js/angular-searchdirective.js
+++ wbc/core/static/js/angular-searchdirective.js
@@ -4,9 +4,102 @@ app.directive('wbcSearch', function() {
         scope: {
             searchTerm: '&'
         },
-        controller: function($scope) {
-            console.log('controller started...');
-        },
-        template: '<h1></h1>'
+        templateUrl: '/wbc-searchdirective.html',
+        controller: ['$scope', '$http', '$location', function($scope, $http, $location){
+            
+            $scope.data = { suggestions: [] };
+            $scope.isLoading = false;
+            $scope.formData = {};
+
+            $scope.selectedSuggestion = null;
+            $scope.selectedSuggestionIdx = -1;
+
+
+            $scope.onSearchChanged = function() {
+                // $scope.noResults = false;
+                if($scope.formData.searchTerm) {
+                    $scope.isLoading = true;
+                    $http({
+                        method: 'GET',
+                        url:  '/autocomplete',
+                        params: {
+                            q: $scope.formData.searchTerm
+                        }
+                    }).success(function(response) {
+
+                        $scope.isLoading = false;
+                        if (response.results.length) {
+                            $scope.data.suggestions = response.results;
+                        } else {
+                            $scope.data.suggestions = [];
+                            $scope.noResults = true;
+                        }
+                    }).error(function(e){
+                        $scope.isLoading = false;
+                    });
+                } else {
+                    $scope.data.suggestions = [];
+                }
+            };
+
+            $scope.submit = function(term){
+                window.location = "/suche/?searchTerm="+ term;
+            };
+
+            $scope.loadDetails = function(result) {
+                window.location.href = result;
+            };
+
+
+            $scope.onKeyDown = function(evt) {
+
+
+                // tab and enter
+                if(evt.keyCode == '9' || evt.keyCode == '13') {
+                    console.log($scope.formData.searchTerm)
+                    if($scope.selectedSuggestionIdx !== -1) {
+                        evt.preventDefault();
+                        $scope.loadDetails($scope.data.suggestions[$scope.selectedSuggestionIdx].internal_link);
+
+                        // window.location = "/suche/?searchTerm="+ $scope.formData.searchTerm;
+
+                    //     if($scope.selectedSuggestionIdx !== -1) {
+                    //         $scope.loadDetails($scope.data.suggestions[$scope.selectedSuggestionIdx]);
+                    //         evt.preventDefault();
+                    //     }
+                    }
+                }
+
+                // arrow down
+                if (evt.keyCode == '40') {
+                    $scope.selectedSuggestionIdx++;
+                }
+
+                // arrow up
+                else if (evt.keyCode == '38') {
+                    $scope.selectedSuggestionIdx--;
+                    evt.preventDefault();
+                }
+
+                if ($scope.selectedSuggestionIdx >= $scope.data.suggestions.length) {
+                    $scope.selectedSuggestionIdx = 0;
+                }
+
+                if($scope.selectedSuggestionIdx == -1) {
+                    $scope.selectedSuggestionIdx = $scope.data.suggestions.length-1;
+                }
+
+                if($scope.selectedSuggestionIdx !== -1) {
+                    var selectedSuggestion = $scope.data.suggestions[$scope.selectedSuggestionIdx];
+                    if(selectedSuggestion) {
+                        // focusAutoCompletionResult(selectedSuggestion);
+                        $scope.selectedSuggestion = selectedSuggestion;
+                    } else {
+                        $scope.selectedSuggestion = null;
+                    }
+                }
+            };
+
+        }]
     };
 });
--- wbc/core/static/js/main.js
+++ wbc/core/static/js/main.js
@@ -1,1 +1,1 @@
-var app = angular.module('wbc', ["checklist-model"]);
+var app = angular.module('wbc', []);
