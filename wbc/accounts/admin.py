2c55162353767463a12867de4b679a9e6d6190aa
Jochen Klar <jochenklar@gmail.com>
2015-11-07 15:06:32 +0100

    Account model

--- wbc/accounts/admin.py
+++ wbc/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account
+
+class AccountAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Account, AccountAdmin)
--- wbc/accounts/models.py
+++ wbc/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return str(self.user.username)
+
+    def __unicode__(self):
+        return unicode(self.__str__())
+
+    class Meta:
+        ordering = ("user",)
+        verbose_name = "Benutzerkonto"
+        verbose_name_plural = "Benutzerkonten"
