b2393d65ce7783afe99828b6dd309e3309431c69
umut0 <umut0815@yahoo.com>
2015-11-09 23:10:07 +0100

    start fix frontpage

--- wbc/core/static/js/utils.js
+++ wbc/core/static/js/utils.js
@@ -32,14 +32,10 @@ function moveScroller(anchor, scroller) {
 }
 
 function scrollCheck(element, scroller){
-    console.log(element);
-    console.log(scroller);
     var move = function() {
         var st = $(scroller).scrollTop();
         var ot = $(element).offset().top+300;
         var el = $('#nav-search');
-        console.log(st)
-        console.log(ot)
         if (st> ot){
             el.addClass('fade-in');
         } else {
