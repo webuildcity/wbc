0e4616afb9b66792786312a96487c0334381fa3e
lundelius <timo@lundelius.com>
2016-01-17 21:48:10 +0100

    migrate merges added

---
+++ wbc/events/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('events', '0010_auto_20160113_1713'),
+        ('events', '0009_remove_event_gallery'),
+    ]
+
+    operations = [
+    ]
