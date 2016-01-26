41c036945c7c46fdf25ee68977c407fcb21abd2b
umut0 <umut0815@yahoo.com>
2016-01-26 11:29:46 -0600

    delete all migrations

--- wbc/core/views.py
+++ wbc/core/views.py
@@ -27,8 +27,6 @@ from guardian.mixins import PermissionRequiredMixin
 
 from django_comments.views.comments import post_comment
 
-from photologue.models import Photo
-
 def feeds(request):
     entities = District.objects.all()
     return render(request, 'core/feeds.html', {
