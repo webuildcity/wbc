114c0e6958e2efddbfac8d6d555ae5d9aea8e759
umut0 <umut0815@yahoo.com>
2016-04-14 15:12:59 +0200

    merge migrations

---
+++ wbc/projects/migrations/0019_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0018_auto_20160121_1302'),
+        ('projects', '0006_auto_20160331_1402'),
+    ]
+
+    operations = [
+    ]
