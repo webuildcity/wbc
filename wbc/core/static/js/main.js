35cb5959bae368ef348038737d4486828f8a52d0
umut0 <umut0815@yahoo.com>
2016-08-20 16:24:19 +0200

    style detail and search, rating widget

--- wbc/core/static/js/main.js
+++ wbc/core/static/js/main.js
@@ -15,8 +15,8 @@ app.controller('DefaultController', function() {
 
 // load the modal and redirection hhandle for subscribe button
 $(document).ready(function(){
-    loadModal('#modal', '.modal-button')
-    loadLoginModal('#login-modal', '.login-button')
+    loadModal('#modal', '.modal-button');
+    loadLoginModal('#login-modal', '.login-button');
     $('.subscribe-button').on('click', function(){
         $.get($(this).data('url'), function(data) {
             if (data.redirect){
@@ -24,5 +24,18 @@ $(document).ready(function(){
                 window.location.reload();
             }
         });
-    })
+    }); 
+
+    var tooltipLocked = false;
+    $('.tooltip-container').mouseover(function(e){
+        if (!tooltipLocked) {
+            $(this).children('.tooltip').css('left', e.offsetX);
+            $(this).children('.tooltip').css('top', e.offsetY);
+            tooltipLocked = true;        
+        }
+        // console.log($(this).children('.tooltip'))
+    });
+    $('.tooltip-container').mouseout(function(e){
+        tooltipLocked = false;
+    });
 }); 
