3992edf4667cf2c12cce764f2252197b0af29d43
umut0 <umut0815@yahoo.com>
2016-08-22 14:54:27 +0200

    lots of small things

--- wbc/tags/views.py
+++ wbc/tags/views.py
@@ -8,14 +8,27 @@ from wbc.stakeholder.models import Stakeholder
 from serializers import *
 from django.shortcuts import render,get_object_or_404
 
+from django.core.paginator import Paginator
 
 def tagview(request, slug):
     t = WbcTag.objects.get(slug__iexact=slug)
+    projects = Project.objects.filter(tags__in=[t]).distinct()
+    stakeholders = Stakeholder.objects.filter(tags__in=[t]).distinct()
+    events = Event.objects.filter(tags__in=[t]).distinct()
+    
+    projects_paginator_full = Paginator(projects, 3)
+    
+    if request.method == 'GET':
+        page = request.GET.get('page')
+        projects_paginator = projects_paginator_full.page(page)
+
+    
+
     return render(request,'tags/tag.html',{
         'tag'             : t,
-        'projects'        : Project.objects.filter(tags__in=[t]).distinct()[:3],
-        'projects_len'    : len(Project.objects.filter(tags__in=[t]).distinct()),
-        'events'          : Event.objects.filter(tags__in=[t]).distinct(),   
-        'stakeholders'    : Stakeholder.objects.filter(tags__in=[t]).distinct()[:3],   
-        'stakeholders_len': len(Stakeholder.objects.filter(tags__in=[t]).distinct()),   
+        'projects'        : projects_paginator,
+        'projects_len'    : len(projects),
+        'events'          : events,   
+        'stakeholders'    : stakeholders,   
+        'stakeholders_len': len(stakeholders),   
     })
