7d4c8c56a1a76718dc53a1c23465e82c92115029
umut0 <umut0815@yahoo.com>
2016-08-26 14:28:46 +0200

    jetzt aber

--- wbc/core/static/js/form_map.js
+++ wbc/core/static/js/form_map.js
@@ -57,13 +57,13 @@ setTimeout(function() {
     map.invalidateSize();
 }, 200);
 
-if (_polygon){
-    if( _polygon.length > 0 ) {
-        var polygon = L.multiPolygon(_polygon);
-        drawnItems.addLayer(polygon);
-    }
+// if (_polygon){
+//     if( _polygon.length > 0 ) {
+//         var polygon = L.multiPolygon(_polygon);
+//         drawnItems.addLayer(polygon);
+//     }
     
-}
+// }
 
 map.on('draw:created', function (e) {
     var type = e.layerType,
